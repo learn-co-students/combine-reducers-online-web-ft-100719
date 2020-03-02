9d7bc98730ceb4875f39bd223e09983eabfb21d2
github username <github email address>
2020-03-01 23:06:53 -0500

    Done.

--- src/components/AuthorInput.js
+++ src/components/AuthorInput.js
@@ -4,42 +4,41 @@ import uuid from 'uuid';
 import { connect } from 'react-redux';
 
 export class AuthorInput extends Component {
-
   state = {
-    authorName: '',
-  }
+    authorName: ''
+  };
 
   handleOnChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
-  }
-
+  };
 
   handleOnSubmit = event => {
     event.preventDefault();
-    const author = {...this.state, id: uuid() };
+    const author = { ...this.state, id: uuid() };
     this.props.addAuthor(author);
     this.setState({
       authorName: ''
     });
-  }
+  };
 
   render() {
-    return(
-      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+    return (
+      <form onSubmit={event => this.handleOnSubmit(event)}>
         <p>
           <input
-            type="text"
-            onChange={(event) => this.handleOnChange(event)}
-            name="authorName"
+            type='text'
+            onChange={event => this.handleOnChange(event)}
+            name='authorName'
             value={this.state.authorName}
-            placeholder="author name" />
+            placeholder='author name'
+          />
         </p>
-        <input type="submit" />
+        <input type='submit' />
       </form>
     );
   }
-};
+}
 
 export default connect(null, { addAuthor })(AuthorInput);
--- src/components/Authors.js
+++ src/components/Authors.js
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Authors extends Component {
-
   render() {
-
-    let authors = this.props.authors.map(author => <li key={author.id}>{author.authorName}</li>);
+    let authors = this.props.authors.map(author => (
+      <li key={author.id}>{author.authorName}</li>
+    ));
 
     return (
       <div>
-        <ul>
-          {authors}
-        </ul>
+        <ul>{authors}</ul>
       </div>
     );
   }
-};
+}
 
 const mapStateToProps = state => {
-  return { authors: state.authors }
-}
+  return { authors: state.authors };
+};
 
 export default connect(mapStateToProps)(Authors);
